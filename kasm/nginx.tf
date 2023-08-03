96a5aa811a41f2aeb66a1833058ceae19869bc81
John Mortensen <jmortensen@powayusd.com>
2023-08-03 09:12:17 -0700

    split files

---
+++ kasm/nginx.tf
@@ -0,0 +1,28 @@
+# nginx.tf
+
+# Define the template for your nginx configuration
+data "template_file" "nginx_conf_template" {
+  template = file("${path.module}/nginx.conf.tpl")
+
+  vars = {
+    subdomain = replace(var.kasm_ec2[count.index], "\\..*$", "")
+  }
+}
+
+# Create an nginx.conf file using the rendered template
+resource "null_resource" "nginx_conf" {
+  count = length(var.kasm_ec2)
+
+  triggers = {
+    template = data.template_file.nginx_conf_template.rendered
+  }
+
+  provisioner "local-exec" {
+    command = "echo '${data.template_file.nginx_conf_template.rendered}' > /tmp/nginx.conf.tpl"
+  }
+
+  provisioner "file" {
+    source      = "/tmp/nginx.conf.tpl"
+    destination = "/etc/nginx/sites-available/"
+  }
+}
