d7f02967e7112e585071a22e53c8c7347bbac7db
John Mortensen <jmortensen@powayusd.com>
2023-08-04 16:40:00 -0700

    plan works on resource

--- kasm/network.tf
+++ kasm/network.tf
@@ -8,14 +8,14 @@ resource "aws_route53_zone" "nighthawkcodingsociety" {
 # create route 53 A records
 resource "aws_route53_record" "kasm_dns" {
   # repeat for each subdomain and relate  kasm_eip 
-  count = length(var.subdomain)
+  for_each = aws_instance.kasm_server
 
   # set zone
   zone_id = aws_route53_zone.nighthawkcodingsociety.id
   # set subdomain using index
-  name    = "${var.subdomain[count.index]}.${var.domain}"
+  name    = each.value.tags["Domain"] # Use the "Domain" tag as the subdomain
   type    = "A"
   ttl     = "300"
   # sets IP, uses index of kasm.eip (elastic ip)
-  records = [aws_eip.kasm_eip[count.index].public_ip]
+  records = [aws_eip.kasm_eip[each.key].public_ip] # Use each.key to index the EIPs
 }
