356fc8bce82e7b1db48a184a7be6d05423938795
Rohan Juneja <aonedtop@gmail.com>
2023-08-31 11:59:09 -0700

    Update migrate script

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,7 @@
-sqlite3 instance/volumes/sqlite.db ".backup instance/volumes/sqlite-backup.db"
+sqlite3 instance/volumes/sqlite.db ".backup instance/volumes/sqlite-backup-1.db"
 export FLASK_APP=main
 export PYTHONPATH=.:$PYTHONPATH  # flask need . to find files
+rm -rf migrate/
 python3 -m flask db init
+python3 -m flask db migrate
 python3 -m flask db upgrade
