84440717725dda7b2e2910a8efd0ac67e4309cd6
Safin Singh <42120904+safinsingh@users.noreply.github.com>
2023-10-17 11:33:03 -0700

    pretty print last N contributions in 10 days

--- api/github.py
+++ api/github.py
@@ -10,14 +10,7 @@ def get_fragment():
         fragment userInfo on User {{
           login
           contributionsCollection(from: "{ten_days_before_now}") {{
-            commitContributionsByRepository {{
-              repository {{
-                name
-              }}
-              contributions(last: 100) {{
-                totalCount
-              }}
-            }}
+            totalCommitContributions
           }}
         }}
     '''
@@ -50,10 +43,18 @@ def send_github_graphql_request(query, api_token):
     response = requests.post('https://api.github.com/graphql', headers=headers, data=payload)
     return response.json()
 
+def parse_graphql_response(response):
+    username_to_commits = {
+      user_data["login"]: user_data["contributionsCollection"]["totalCommitContributions"]
+      for user_data in response["data"].values()
+    }
+    return username_to_commits
+
 usernames = ['safinsingh', 'rjawesome', 'vardaansinha']
 api_token = os.environ["GITHUB_TOKEN"]
 
 query = generate_github_graphql_query(usernames)
 response = send_github_graphql_request(query, api_token)
+stats = parse_graphql_response(response)
 
-print(json.dumps(response, indent=4))
+print(stats)
